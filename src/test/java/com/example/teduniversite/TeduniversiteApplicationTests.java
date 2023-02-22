a106fb8c117c35dc480b03f4d5773179d9a9de4d
fidebenslimen <fidabenslimen3@gmail.com>
2023-02-22 11:42:57 +0100

    first git lab

---
+++ src/test/java/com/example/teduniversite/TeduniversiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teduniversite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeduniversiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
