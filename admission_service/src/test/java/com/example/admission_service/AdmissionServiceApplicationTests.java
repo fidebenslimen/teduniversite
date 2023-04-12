f61315beeb1ed3c08a1108f39cc54fa635f92f72
Ghada Soltani <ghada.soltani@ihec.ucar.Tn>
2023-04-12 03:48:47 +0200

    Merge remote-tracking branch 'origin/controller'

---
+++ admission_service/src/test/java/com/example/admission_service/AdmissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admission_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
